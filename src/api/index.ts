317cea13a1b681e7f145f69008a439c6b3703b73
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-01-08 08:46:16 +0000

    building api service

---
+++ src/api/index.ts
@@ -0,0 +1,46 @@
+import { apiOptions, matchesType } from "@/types";
+
+const options:apiOptions = {
+    next: { revalidate: 30 },
+    headers:{
+        'X-Auth-Token': process.env.SOCCER_API_TOKEN,
+        'Content-Type': 'application/json'
+    }
+}
+
+const todaysDate = new Date();
+//const getDateMonth = todaysDate.getMonth() + 1;
+const getDateMonth = new Date(todaysDate.getTime());
+getDateMonth.setDate(todaysDate.getDate() - 1);
+const year = getDateMonth.getFullYear();
+const month = String(getDateMonth.getMonth() + 1).padStart(2, '0');
+const day = String(getDateMonth.getDate() + 1).padStart(2, '0');
+
+const yesterday = [year, month, day].join('-');
+
+export const getMatchesfootball = async () => {
+  const matchData = await fetch('https://api.football-data.org/v4/matches',options)
+  return matchData.json()
+}
+
+export const getMatchesFootballFinished = async () => {
+    const matchData = await fetch(`https://api.football-data.org/v4/matches?date=&{yesterday}`,options);
+    return matchData.json()
+}
+
+// export const getNewsInfo = async () => {
+//     const newsData = await fetch(`https://newsapi.org/v2/everything?q=football&apiKey=${process.env.NEWS_API_TOKEN}`,{next:{revalidate:30}});
+//     return newsData.json();
+// }
+
+export const getNewsInfo = async () => {
+  const newsData = await fetch(`https://newsapi.org/v2/everything?apiKey=${process.env.API_TOKEN_NEWS}&q=soccer&pageSize=5`,{next:{revalidate:30}})
+  return newsData.json()
+}
+
+export const filterLeague = async (filterData:string) => {
+    const getEnglishLeague = await getMatchesfootball();
+    const filterPremierLeague:matchesType[] = getEnglishLeague?.matches;
+    const getData = filterPremierLeague.filter((data) => data.competition.name === filterData);
+    return getData;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 
 export default function Home() {
+  const date = new Date();
+  const dateConverted = date.toDateString();
   return (
-    <section className="flex text-textPrimary">
-      <div className="">
-        <h1 className="font-bold uppercase">Matches</h1>
+    <section className="px-2 md:px-4 md:w-[600px]">
+      <div className="flex justify-between items-center mb-4 md:mb-2">
+        <h1 className="font-bold uppercase text-md md:text-xl">Matches</h1>
+      </div>
+      <div className="rounded-md px-4 py-0 md:py-1 bg-slate-600 text-textPrimary text-sm">
+        <p className="">{ dateConverted }</p>
       </div>
     </section>
   )
